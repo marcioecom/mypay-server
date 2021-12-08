2485c1c0841df049827070f6cc69fcf74fdc37c0
Marcio Junior <marcio.mj70@gmail.com>
2021-12-08 18:20:22 -0300

    style: replace indentation

--- src/useCases/authenticateUser/AuthenticateUser.ts
+++ src/useCases/authenticateUser/AuthenticateUser.ts
@@ -3,39 +3,39 @@ import { sign } from "jsonwebtoken"
 import { prisma } from "../../database/prisma"
 
 interface IAuthUser {
-	email: string;
-	password: string;
+  email: string;
+  password: string;
 }
 
 class AuthenticateUser {
-	async execute({ email, password }: IAuthUser) {
-		const user = await prisma.user.findUnique({
-			where: {
-				email
-			}
-		});
+  async execute({ email, password }: IAuthUser) {
+    const user = await prisma.user.findUnique({
+      where: {
+        email
+      }
+    });
 
-		if (!user) {
-			throw new Error("User does not exists");
-		}
+    if (!user) {
+      throw new Error("User does not exists");
+    }
 
-		const passwordMatch = await compare(password, user.password);
+    const passwordMatch = await compare(password, user.password);
 
-		if (!passwordMatch) {
-			throw new Error("Email/Password incorrect");
-		}
+    if (!passwordMatch) {
+      throw new Error("Email/Password incorrect");
+    }
 
-		const token = sign(
-			{ email: user.email },
-			process.env.SECRET,
-			{
-				subject: user.id,
-				expiresIn: "1d",
-			}
-		);
+    const token = sign(
+      { email: user.email },
+      process.env.SECRET,
+      {
+        subject: user.id,
+        expiresIn: "1d",
+      }
+    );
 
-		return token;
-	}
+    return token;
+  }
 }
 
 export { AuthenticateUser }
