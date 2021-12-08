8d44ec98d2357ebd3c025729bef22b8667429dc8
Marcio Junior <marcio.mj70@gmail.com>
2021-12-08 17:46:26 -0300

    feat: authenticate user

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
 import { usersRoutes } from "./users.routes";
+import { AuthenticateUserController } from "../useCases/authenticateUser/AuthenticateUserController";
 
 const routes = Router();
 
+const authenticateUserController = new AuthenticateUserController();
+
+routes.post("/login", authenticateUserController.handle)
 routes.use("/users", usersRoutes)
 
 export { routes };
+
