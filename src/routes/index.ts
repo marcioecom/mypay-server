819641df97d77f90f4c97753025af50d454011f0
Marcio Junior <marcio.mj70@gmail.com>
2021-12-11 04:06:57 -0300

    feat: set new password

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,14 +2,17 @@ import { Router } from "express";
 import { usersRoutes } from "./users.routes";
 import { AuthenticateUserController } from "../useCases/authenticateUser/AuthenticateUserController";
 import { RequestResetPasswordController } from "../useCases/requestResetPassword/RequestResetPasswordController";
+import { ResetPasswordController } from "../useCases/resetPassword/ResetPasswordController";
 
 const routes = Router();
 
 const authenticateUserController = new AuthenticateUserController();
 const requestResetPasswordController = new RequestResetPasswordController();
+const resetPasswordController = new ResetPasswordController()
 
 routes.post("/login", authenticateUserController.handle)
 routes.post("/request-reset-password", requestResetPasswordController.handle)
+routes.post("/reset-password", resetPasswordController.handle)
 
 routes.use("/users", usersRoutes)
 
