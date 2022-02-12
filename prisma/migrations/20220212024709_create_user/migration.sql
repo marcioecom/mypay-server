8ccdec765fd2ad123f56dc7313a0af03a2378803
Marcio Junior <marcio.mj70@gmail.com>
2022-02-11 23:49:10 -0300

    fix(migration): rerun migrations

--- prisma/migrations/20220212024709_create_user/migration.sql
+++ prisma/migrations/20220212024709_create_user/migration.sql
@@ -2,7 +2,7 @@
 CREATE TABLE "users" (
     "id" TEXT NOT NULL,
     "firstName" TEXT NOT NULL,
-    "lastName" TEXT NOT NULL;
+    "lastName" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
 
